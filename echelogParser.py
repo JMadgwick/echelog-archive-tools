439f39a781c34c0604d38b53bbaa94cfacf7849d
JMadgwick <32958136+JMadgwick@users.noreply.github.com>
2021-09-11 23:21:17 +0100

    Skip blank messages, use ISO datetime and not local Locale for now

--- echelogParser.py
+++ echelogParser.py
@@ -25,6 +25,7 @@ locale.setlocale(locale.LC_ALL, '')   # use user's preferred locale
 argPath = 'echelog/'
 argHide = True
 outFile = open(argPath + 'output.log', 'w')
+dateTimeFormat = '%Y-%m-%dT%H:%M:%S'
 
 files = os.listdir(argPath)
 #Filter out non html files and remove extension to allow easy sorting
@@ -36,6 +37,7 @@ for webpage in pages:
     with open(argPath + '/' + webpage + '.html') as doc:
         soup = BeautifulSoup(doc, 'html.parser')
 
+    print(webpage, end="\r")
     ircText = soup.body.pre
     ircLines = []
 
@@ -43,10 +45,12 @@ for webpage in pages:
         time = str(line.contents[0])[1:9]
         msgType = line.contents[1]['class'][0]
         message = line.contents[1].get_text()
-        ircLines.append([time,msgType,message])
+        #Skip blank messages
+        if message != '':
+            ircLines.append([time,msgType,message])
     
     for line in ircLines:
         if line[1] == 'd' or not argHide:
-            outFile.write(datetime.strptime(webpage[:10] + line[0],'%Y-%m-%d%H:%M:%S').strftime('%c ') + line[2])
+            outFile.write(datetime.strptime(webpage[:10] + line[0],'%Y-%m-%d%H:%M:%S').strftime(dateTimeFormat) + ' ' + line[2] + '\n')
 outFile.close()
 
