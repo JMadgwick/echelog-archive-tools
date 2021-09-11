01a43008b21d06d5a3b3fd040fba1172b636eacb
JMadgwick <32958136+JMadgwick@users.noreply.github.com>
2021-09-11 23:18:26 +0100

    Improve anchor string transformation, ignore empty pages Fixes problem where final page wouldn't be parsed correctly as it had '&nbsp' on one side only. Ignore empty pages as these are a waste of space.

--- echelogScraper.py
+++ echelogScraper.py
@@ -35,13 +35,17 @@ for i in range(1,10):
     anchor = soup.find_all(string="<")[0].parent #find_all gives up the string itself, we need the <a> which is the parent
     nextPage = anchor['href']
     print('Next page is: ' + nextPage)
-    
-    #Rewrite URL to fetch as last fetched (including redirects) but with page changed
+
+    #Rewrite URL to insert web.archive.org URL from last fetched page (including redirects)
     URL = resultURL[:resultURL.rindex('/') + 1] + nextPage
-    
+
     #To get date, use anchor parent (menu <div>) and fixed location
-    pageDate = anchor.parent.contents[2][3:-2] #Plus trim '&nbsp' chars and spaces from start and end of string
+    pageDate = anchor.parent.contents[2].replace('\xa0','').strip() #Remove '&nbsp' chars and trim spaces from start and end of string
     outputFilename = datetime.strptime(pageDate,'%B %d, %Y').date().strftime('%Y-%m-%d') + '.html'
-    
+
+    #Check not an empty page
+    if soup.pre.get_text() == 'no log file for date':
+        print('WARNING: Nothing logged on ' + pageDate)
+
     open('echelog/' + outputFilename, 'wb').write(result.content)
     print('Saved file as: ' + outputFilename)
