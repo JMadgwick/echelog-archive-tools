f6e057553d6da3b62d7d0ec0840fbf3482e71b18
JMadgwick <32958136+JMadgwick@users.noreply.github.com>
2021-09-25 18:10:03 +0100

    Add arguments and help, check for multiple empty pages, check for final page

--- echelogScraper.py
+++ echelogScraper.py
@@ -15,37 +15,76 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from bs4 import BeautifulSoup
-import requests
+import requests,argparse,locale
+from pathlib import Path
 from datetime import datetime
 
-startingURL = 'https://web.archive.org/web/20200531221309/https://echelog.com/logs/browse/????'
-URL = startingURL
+def validatedURL(URL):
+    archivePos = URL.index('https://web.archive.org/web/')
+    echelogPos = URL.index('https://echelog.com/logs/browse/')
+    if ((archivePos == 0) and (echelogPos == 43)):
+        return URL
+    else:
+        raise ValueError()
 
-for i in range(1,10):
-    print('Downloading page... ')
+#Set locale - used for output message date format
+locale.setlocale(locale.LC_ALL, '')
+
+parser = argparse.ArgumentParser(description='Utility to scrape the WayBackMachine archived version of echelog and download pages (containing IRC logs) from a specific channel. The URL supplied must be to an IRC log page, pages/dates before this will then be fetched')
+parser.add_argument('initialURL', type=validatedURL, help='URL to start scraping from. Must be in the form https://web.archive.org/web/{datetime}/https://echelog.com/logs/browse/{channel}/{id}')
+parser.add_argument('-p', '--pages', type=int, help='Maximum number of pages (days) to download')
+parser.add_argument('-na', '--noabort', action='store_true', help="Don't abort when no logs are found for a one month period")
+parser.add_argument('outputPath', type=Path, help='Path/directory to write files into. Must already exist, any conflicting files will be overwritten')
+args = parser.parse_args()
+
+if (Path.exists(args.outputPath)):
+    print ("Writing to '" + str(Path.absolute(args.outputPath)) + "', any conflicting files will be overwritten")
+    outputPath = str(args.outputPath)
+else:
+    print ("Path/directory '" + str(Path.absolute(args.outputPath)) + "' does not exist!")
+    exit()
+
+URL = args.initialURL
+limit = args.pages or 6000
+noLogCheck = 30
+
+for i in range(1,limit):
+    print('Downloading page...', end="\r")
     result = requests.get(URL)
     resultURL = result.url
     print('Fetched: ' + resultURL)
     webpage = result.text
 
-    soup = BeautifulSoup(webpage, 'html.parser')
+    #Check for the final (earliest) page, which is blank and empty
+    if (webpage == ''):
+        print('Final page reached. Nothing more to fetch.')
+        break
+
+    soup = BeautifulSoup(webpage, 'lxml')
 
     #To get next URL, find the previous page anchor - shown as '<' twice (at top and bottom of the page)
     #anchor = soup.body.contents[3].contents[5] #Hardcoded location will not work if Wayback toolbar is inserted
     anchor = soup.find_all(string="<")[0].parent #find_all gives up the string itself, we need the <a> which is the parent
     nextPage = anchor['href']
-    print('Next page is: ' + nextPage)
 
     #Rewrite URL to insert web.archive.org URL from last fetched page (including redirects)
     URL = resultURL[:resultURL.rindex('/') + 1] + nextPage
 
     #To get date, use anchor parent (menu <div>) and fixed location
     pageDate = anchor.parent.contents[2].replace('\xa0','').strip() #Remove '&nbsp' chars and trim spaces from start and end of string
-    outputFilename = datetime.strptime(pageDate,'%B %d, %Y').date().strftime('%Y-%m-%d') + '.html'
+    pageDateParsed = datetime.strptime(pageDate,'%B %d, %Y').date()
+    outputFilename = pageDateParsed.strftime('%Y-%m-%d') + '.html'
 
-    #Check not an empty page
+    #Check not an empty page, don't save these
     if soup.pre.get_text() == 'no log file for date':
-        print('WARNING: Nothing logged on ' + pageDate)
+        print('WARNING: Nothing logged on ' + pageDate + '. Not saving this page.')
+        noLogCheck -= 1
+        if ((noLogCheck == 0) and not args.noabort):
+            print("No logs found for a one month period. Aborting. Use '-na' flag to disable this.")
+            break
+    else:
+        open(outputPath + '/' + outputFilename, 'wb').write(result.content)
+        print('Saved page for ' + pageDateParsed.strftime('%x') + ' as: ' + outputFilename)
+        noLogCheck = 30
 
-    open('echelog/' + outputFilename, 'wb').write(result.content)
-    print('Saved file as: ' + outputFilename)
+print('Complete')
