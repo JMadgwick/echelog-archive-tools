884ddc4305f3f816f3e11b088f2865ba53ccfa50
JMadgwick <32958136+JMadgwick@users.noreply.github.com>
2021-09-25 18:08:03 +0100

    Add arguments and help, progress bar, improve performance by using lxml and a filter

--- echelogParser.py
+++ echelogParser.py
@@ -14,43 +14,55 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from bs4 import BeautifulSoup
-import os
+from bs4 import BeautifulSoup, SoupStrainer
+import os,locale,argparse
 from datetime import datetime
+from alive_progress import alive_bar
 
-import locale
-locale.setlocale(locale.LC_ALL, '')   # use user's preferred locale
+parser = argparse.ArgumentParser(description='Simple utility to parse a directory of Echelog HTML IRC chat log files, extract the IRC chat logs and store them in a single file using a standard format. By default, ISO format is used for the timestamp (eg. "2011-02-13T01:55:34").')
+parser.add_argument('directory', help='Path to directory containing Echelog HTML files')
+parser.add_argument('-l', '--local', action='store_true', help='Use local locale based timestamp instead of ISO format')
+parser.add_argument('-a', '--all', action='store_true', help='Include all IRC messages. By default, only chat message are included and not join/part etc. Note that Echelog used different formats for these over the years')
+parser.add_argument('output', type=argparse.FileType('w'), help='File to write output into')
+args = parser.parse_args()
 
+if args.local:
+    #Set locale to local locale
+    locale.setlocale(locale.LC_ALL, '')
+    dateTimeFormat = '%c'
+else:
+    dateTimeFormat = '%Y-%m-%dT%H:%M:%S'
 
-argPath = 'echelog/'
-argHide = True
-outFile = open(argPath + 'output.log', 'w')
-dateTimeFormat = '%Y-%m-%dT%H:%M:%S'
-
-files = os.listdir(argPath)
+files = os.listdir(args.directory)
 #Filter out non html files and remove extension to allow easy sorting
 pages = [fn[:-5] for fn in files if fn[-5:] == '.html']
+#Sort logs into date order
 pages.sort()
 
-for webpage in pages:
-    #Open file and parse HTML
-    with open(argPath + '/' + webpage + '.html') as doc:
-        soup = BeautifulSoup(doc, 'html.parser')
+#Filter to parse only the <pre> tag, not the rest of the document - this improves performance
+preFilter = SoupStrainer("pre")
 
-    print(webpage, end="\r")
-    ircText = soup.body.pre
-    ircLines = []
+with alive_bar(len(pages), title='Processing HTML files', spinner='classic') as bar:
+    for webpage in pages:
+        #Open file and parse HTML, use lxml to further improve performance
+        with open(args.directory + '/' + webpage + '.html') as doc:
+            soup = BeautifulSoup(doc, 'lxml', parse_only=preFilter)
 
-    for line in ircText.find_all('div'):
-        time = str(line.contents[0])[1:9]
-        msgType = line.contents[1]['class'][0]
-        message = line.contents[1].get_text()
-        #Skip blank messages
-        if message != '':
-            ircLines.append([time,msgType,message])
-    
-    for line in ircLines:
-        if line[1] == 'd' or not argHide:
-            outFile.write(datetime.strptime(webpage[:10] + line[0],'%Y-%m-%d%H:%M:%S').strftime(dateTimeFormat) + ' ' + line[2] + '\n')
-outFile.close()
+        bar()
+        bar.text('[File: '+ webpage + ']')
+        ircText = soup#.body.pre
+        ircLines = []
 
+        for line in ircText.find_all('div'):
+            time = str(line.contents[0])[1:9]
+            msgType = line.contents[1]['class'][0]
+            message = line.contents[1].get_text()
+            #Skip blank messages
+            if message != '':
+                ircLines.append([time,msgType,message])
+
+        for line in ircLines:
+            if line[1] == 'd' or args.all:
+                args.output.write(datetime.strptime(webpage[:10] + line[0],'%Y-%m-%d%H:%M:%S').strftime(dateTimeFormat) + ' ' + line[2] + '\n')
+
+args.output.close()
